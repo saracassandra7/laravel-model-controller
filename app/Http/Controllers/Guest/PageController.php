7ef0797cc1d1b81e500692320ff74babe539cb6f
saracassandra7 <saracassandra7@gmail.com>
2023-01-09 17:36:09 +0100

    [add] movies cards

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
 {
     public function index(){
         $movies = Movie::all();
-        dd($movies);
+        //dd($movies);
 
         return view('home', compact('movies'));
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,23 @@
 
 @section('content')
 <div class="container">
-    {{$saluto}}
+    <h1 class="text-center m-3">Movies</h1>
+    <div class="row">
+      @foreach ( $movies as $movie )
+       <div class="col-4 mb-3">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">{{$movie->title}}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">{{$movie->original_title}}</h6>
+              <p class="card-text">Release date: {{$movie->date}}</p>
+              {{-- <a href="#" class="card-link">Card link</a> --}}
+            </div>
+          </div>
+
+       </div>
+
+      @endforeach
+    </div>
 </div>
 
 @endsection
